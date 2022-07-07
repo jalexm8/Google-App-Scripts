93128c425b7a8bd50a4b86ecc9feecf50d7308fa
jalexm8 <jack.alexander1008@gmail.com>
2022-07-07 22:58:51 +0100

    pto system part 2

--- pto/pto_form.js
+++ pto/pto_form.js
@@ -15,11 +15,42 @@ function formResponsesToArray() {
     return userResponses;
   }
   
-function onFormSubmit(e) {
+  function errorChecking(userResponses) {
+    var startDate = new Date(userResponses['start date']);
+    var endDate = new Date(userResponses['end date']);
+    var errorMsg = "";
+  
+    if ( startDate.getTime() > endDate.getTime() ) {
+      errorMsg += "ERROR: Start date cannot be ahead of end date.\n";
+    }
+    
+    Logger.log(errorMsg);
+  
+    return errorMsg;
+  }
+  
+  function daysBetweenStartAndEndDate(userResponses) {
+    var startDate = new Date(userResponses['start date']);
+    var endDate = new Date(userResponses['end date']);
+    var timeBetween = endDate.getTime() - startDate.getTime();
+    var daysBetween = timeBetween / (1000 * 3600 * 24);
+  
+    return daysBetween;
+  }
+  
+  function onFormSubmit(e) {
     var userResponses = formResponsesToArray();
+    var ptoDaysTaken = daysBetweenStartAndEndDate(userResponses);
+    var errorMsg = errorChecking(userResponses);
   
     Logger.log(userResponses['email'])
     Logger.log(userResponses['start date']);
     Logger.log(userResponses['end date']);
+    Logger.log(ptoDaysTaken);
+  
+  
+    if ( errorMsg.length > 0 ) {
+      Logger.log(errorMsg);
+    }
   }
   
